8531947808431cc53c1f5a2e07b1a94a7f9ac11d
Valentin <gerg20000@mail.ru>
2020-07-19 02:35:11 +0300

    Layout
    Сделал Layout и реализовал скрытие меню

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+//import Categories from '../views/Categories.vue'
 
 Vue.use(VueRouter)
 
@@ -8,13 +8,57 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
-  }/*,
+    meta: {layout: 'main'},
+    component: () => import('../views/Home.vue') 
+  },
   {
-    path: '/about',
-    name: 'About',
-    component: () => import( '../views/About.vue')
-  }*/
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import( '../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import( '../views/Register.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import( '../views/Categories.vue')
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: {layout: 'main'},
+    component: () => import( '../views/DetailRecord.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import( '../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import( '../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import( '../views/Profile.vue')
+  },
+  {
+    path: '/Record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import( '../views/Record.vue')
+  }
 ]
 
 const router = new VueRouter({
